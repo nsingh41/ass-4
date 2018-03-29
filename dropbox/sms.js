eac6f339d2df12a48b9f98b7941728498cc40bc6
Navjot <kanda.navjotsingh@gmail.com>
2018-03-29 00:06:32 +0000

    you-commit

--- dropbox/sms.js
+++ dropbox/sms.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const accountSid = 'accountSid'
-const authToken = 'authToken'
+const accountSid = 'AC6bca12313b4e1c62ab86293b163fc8b2'
+const authToken = '1ba6036cff91a64f4ff01a53e54c0e97'
 const client = require('twilio')(accountSid, authToken)
 
 module.exports.sms = (event, context, callback) => {
@@ -10,7 +10,7 @@ module.exports.sms = (event, context, callback) => {
 
   client.messages.create(
     {
-      to: '',
+      to: '+17789524383',
       from: '+16042275029',
       body: 'file added to s3'
     },
--- dropbox/upload.js
+++ dropbox/upload.js
@@ -3,6 +3,11 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3()
+const lambda = new AWS.Lambda({
+  region: 'us-west-2'
+})
+
 module.exports.upload = (event) => {
   event.Records.forEach((record) => {
     const filename = record.s3.object.key;
@@ -24,7 +29,7 @@ eventTime: eventTime
         return;
       }
       let twilio = {
-        FunctionName: 'navjot-dev-message',
+        FunctionName: 'navjot-dev-sms',
         Payload: JSON.stringify({ filename: filename })
       }
       const lambda = require('aws-lambda-invoke');
